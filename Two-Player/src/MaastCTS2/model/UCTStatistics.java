4232cebe2a4566830b3e321e00fbfe9dbb4d2987
lvanparijs <lvanparijs@gmail.com>
2017-04-24 13:32:16 +0200

    Created UCTStatistics Object

--- Two-Player/src/MaastCTS2/model/UCTStatistics.java
+++ Two-Player/src/MaastCTS2/model/UCTStatistics.java
@@ -1,5 +1,7 @@
 package MaastCTS2.model;
 
+import ontology.Types;
+
 /**
  * Created by I6091912 on 24-4-2017.
  */
@@ -10,13 +12,16 @@ public class UCTStatistics {
 
     public UCTStatistics[] children;
 
+    public Types.ACTIONS action;//The associated action with this statistic
 
-    public UCTStatistics(){
+    public UCTStatistics(Types.ACTIONS action){
+        this.action = action;
         this.s = 0;
         this.v = 0;
     }
 
-    public UCTStatistics(double s, double v){
+    public UCTStatistics(double s, double v, Types.ACTIONS action){
+        this.action = action;
         this.s = s;
         this.v = v;
     }
