988d2e5d30230527ad97997c8444505e30d78cb1
Lucas Vanparijs <lvanparijs@gmail.com>
2017-04-24 15:37:04 +0200

    Update UCTStatistics.java

--- Two-Player/src/MaastCTS2/model/UCTStatistics.java
+++ Two-Player/src/MaastCTS2/model/UCTStatistics.java
@@ -9,25 +9,49 @@ public class UCTStatistics {
 
     public double s;
     public double v;
+    
+    public boolean terminal
 
     public UCTStatistics[] children;
 
     public Types.ACTIONS action;//The associated action with this statistic
 
-    public UCTStatistics(Types.ACTIONS action){
-        this.action = action;
-        this.s = 0;
-        this.v = 0;
+    public UCTStatistics(Types.ACTIONS action, boolean terminal){
+        this(0,0,action,terminal);
     }
 
-    public UCTStatistics(double s, double v, Types.ACTIONS action){
+    public UCTStatistics(double s, double v, Types.ACTIONS action, boolean terminal){
         this.action = action;
         this.s = s;
         this.v = v;
+        this.terminal = terminal;
+        
+        if(!terminal) {
+            //Creates children if this is not terminal, since I only need 2 layers i can pass in !terminal in the next constructor  
+            this.children = new UCTStatistics[Types.ACTIONS.values().length];
+            int curIndex = 0;
+            for (Types.ACTIONS a : Types.ACTIONS.values()) {
+                children[curIndex] = new UCTStatistics(a,!terminal);
+            }
+        }
     }
 
     public void update(double score){
 
+    }
+    
+    public double getAvgScore(){
+        double sumS = 0;
+        double sumV = 0;
+        if(children == null){
+            System.out.println("Terminal Node reached");
+        }else {
+            for (UCTStatistics stat : children) {
+                sumS += stat.s;
+                sumV += stat.v;
+            }
+        }
+        return sumS/sumV;
     }
 
 }
